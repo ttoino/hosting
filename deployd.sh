31fb51e1c555d9433e3a686c3e8b8a98567085cc
toino <joaoapereira21@hotmail.com>
2023-08-16 23:13:52 +0100

    Add time stamp to logs

--- deployd.sh
+++ deployd.sh
@@ -5,5 +5,5 @@ kill -9 "$(cat deploy.pid)"
 eval "$(ssh-agent -s)"
 ssh-add
 
-nohup ./deploy.sh >logs/deploy.log &
+(nohup ./deploy.sh | ts "[%Y-%m-%dT%H:%M:%S]") >logs/deploy.log &
 echo $! >deploy.pid
