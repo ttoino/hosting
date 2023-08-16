d9f38e8c6419264b61af2d783060b6e5d3bbdab8
toino <joaoapereira21@hotmail.com>
2023-08-16 23:06:12 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,23 @@ DOCKER=docker
 mkfifo $FIFO
 
 git pull
-docker compose down -v
+docker compose down -v --remove-orphans
 docker compose up -d --build registry
 docker compose pull
 docker compose up -d --build
 
 while true; do
-    read -ra line
+    read -r command arg <"$FIFO"
 
-    if [[ "${line[0]}" == "$GIT" ]]; then
+    if [[ "$command" == "$GIT" ]]; then
         git pull
-        docker compose down -v
+        docker compose down -v --remove-orphans
         docker compose up -d --build
-    elif [[ "${line[0]}" == "$DOCKER" ]]; then
-        docker compose pull "${line[1]}"
-        docker compose down -v "${line[1]}"
+    elif [[ "$command" == "$DOCKER" ]]; then
+        docker compose pull "$arg"
+        docker compose down -v "$arg"
         docker compose up -d --build
+    else 
+        echo "Unknown command: $command $arg"
     fi
-done <$FIFO
+done
